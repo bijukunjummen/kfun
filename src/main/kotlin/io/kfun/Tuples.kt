76a61d92882e93912e913ac223f3cd8178c28416
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-12-29 16:42:27 -0800

    Adds in tuples upto 5 elements

---
+++ src/main/kotlin/io/kfun/Tuples.kt
@@ -0,0 +1,15 @@
+package io.kfun
+
+object Tuple {
+    fun <A> of(_1: A): Tuple1<A> = Tuple1(_1)
+    fun <A, B> of(_1: A, _2: B): Tuple2<A, B> = Tuple2(_1, _2)
+    fun <A, B, C> of(_1: A, _2: B, _3: C): Tuple3<A, B, C> = Tuple3(_1, _2, _3)
+    fun <A, B, C, D> of(_1: A, _2: B, _3: C, _4: D): Tuple4<A, B, C, D> = Tuple4(_1, _2, _3, _4)
+    fun <A, B, C, D, E> of(_1: A, _2: B, _3: C, _4: D, _5: E): Tuple5<A, B, C, D, E> = Tuple5(_1, _2, _3, _4, _5)
+}
+
+data class Tuple1<out A>(val _1: A)
+data class Tuple2<out A, out B>(val _1: A, val _2: B)
+data class Tuple3<out A, out B, out C>(val _1: A, val _2: B, val _3: C)
+data class Tuple4<out A, out B, out C, out D>(val _1: A, val _2: B, val _3: C, val _4: D)
+data class Tuple5<out A, out B, out C, out D, out E>(val _1: A, val _2: B, val _3: C, val _4: D, val _5: E)
---
+++ src/test/kotlin/io/kfun/TupleTests.kt
@@ -0,0 +1,117 @@
+package io.kfun
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class TupleTests {
+
+    @Test
+    fun `should be able to create a tuple of 1 element`() {
+        val tup = Tuple1("elem1")
+        assertThat(tup._1).isEqualTo("elem1")
+
+        val (e1) = tup
+        assertThat(e1).isEqualTo("elem1")
+    }
+
+    @Test
+    fun `should be able to create Tuple1 with a utility method`() {
+        val tup = Tuple.of("elem1")
+
+        assertThat(tup._1).isEqualTo("elem1")
+    }
+
+    @Test
+    fun `should be able to create a tuple with 2 elements of different types`() {
+        val tup = Tuple2("elem1", "elem2")
+        assertThat(tup._1).isEqualTo("elem1")
+        assertThat(tup._2).isEqualTo("elem2")
+
+        val (e1, e2) = tup
+
+        assertThat(e1).isEqualTo("elem1")
+        assertThat(e2).isEqualTo("elem2")
+    }
+
+    @Test
+    fun `should be able to create Tuple2 with a utility method`() {
+        val tup = Tuple.of("elem1", "elem2")
+        assertThat(tup._1).isEqualTo("elem1")
+        assertThat(tup._2).isEqualTo("elem2")
+    }
+
+    @Test
+    fun `should be able to create a tuple with 3 elements of different types`() {
+        val tup = Tuple3("elem1", "elem2", "elem3")
+        assertThat(tup._1).isEqualTo("elem1")
+        assertThat(tup._2).isEqualTo("elem2")
+        assertThat(tup._3).isEqualTo("elem3")
+
+        val (e1, e2, e3) = tup
+
+        assertThat(e1).isEqualTo("elem1")
+        assertThat(e2).isEqualTo("elem2")
+        assertThat(e3).isEqualTo("elem3")
+
+    }
+
+    @Test
+    fun `should be able to create Tuple3 with a utility method`() {
+        val tup = Tuple.of("elem1", "elem2", "elem3")
+        assertThat(tup._1).isEqualTo("elem1")
+        assertThat(tup._2).isEqualTo("elem2")
+        assertThat(tup._3).isEqualTo("elem3")
+    }
+
+    @Test
+    fun `should be able to create a tuple with 4 elements each of different type`() {
+        val tup = Tuple4("elem1", "elem2", "elem3", "elem4")
+        assertThat(tup._1).isEqualTo("elem1")
+        assertThat(tup._2).isEqualTo("elem2")
+        assertThat(tup._3).isEqualTo("elem3")
+        assertThat(tup._4).isEqualTo("elem4")
+
+        val (e1, e2, e3, e4) = tup
+        assertThat(e1).isEqualTo("elem1")
+        assertThat(e2).isEqualTo("elem2")
+        assertThat(e3).isEqualTo("elem3")
+        assertThat(e4).isEqualTo("elem4")
+
+    }
+
+    @Test
+    fun `should be able to create Tuple4 with a utility method`() {
+        val tup = Tuple.of("elem1", "elem2", "elem3", "elem4")
+        assertThat(tup._1).isEqualTo("elem1")
+        assertThat(tup._2).isEqualTo("elem2")
+        assertThat(tup._3).isEqualTo("elem3")
+        assertThat(tup._4).isEqualTo("elem4")
+    }
+
+    @Test
+    fun `should be able to create a tuple with 5 elements each of different type`() {
+        val tup = Tuple5("elem1", "elem2", "elem3", "elem4", "elem5")
+        assertThat(tup._1).isEqualTo("elem1")
+        assertThat(tup._2).isEqualTo("elem2")
+        assertThat(tup._3).isEqualTo("elem3")
+        assertThat(tup._4).isEqualTo("elem4")
+        assertThat(tup._5).isEqualTo("elem5")
+        
+        val (e1, e2, e3, e4, e5) = tup
+        assertThat(e1).isEqualTo("elem1")
+        assertThat(e2).isEqualTo("elem2")
+        assertThat(e3).isEqualTo("elem3")
+        assertThat(e4).isEqualTo("elem4")
+        assertThat(e5).isEqualTo("elem5")
+    }
+
+    @Test
+    fun `should be able to create Tuple5 with a utility method`() {
+        val tup = Tuple.of("elem1", "elem2", "elem3", "elem4", "elem5")
+        assertThat(tup._1).isEqualTo("elem1")
+        assertThat(tup._2).isEqualTo("elem2")
+        assertThat(tup._3).isEqualTo("elem3")
+        assertThat(tup._4).isEqualTo("elem4")
+        assertThat(tup._5).isEqualTo("elem5")
+    }
+}
